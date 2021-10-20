4bb6da23d336db0ea5afa88855b16036403ab0b1
Karl Edenstål <karl.edenstal@rhyme.se>
2021-10-20 22:29:01 +0200

    add dice roller

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-require('dotenv').config();
+require("dotenv").config();
 const responses = require("./src/responses");
 const discordBotkit = require("botkit-discord");
 
 const configuration = {
-  token: process.env.TOKEN
+  token: process.env.TOKEN,
 };
 
 const discordBot = discordBotkit(configuration);
 
+var dice = {
+  sides: 6,
+  amount: 1,
+  roll: function (sides = this.sides, amount = this.amount) {
+    var a = [];
+    var result = 0;
+
+    for (var i = 0; i < amount; i++) {
+      a[i] = Math.floor(Math.random() * sides) + 1;
+      console.log("dice " + i + " rolled: " + a[i]);
+      result += a[i];
+    }
+
+    return {
+      allDice: a,
+      result,
+    };
+  },
+};
+
 discordBot.on("ready", () => {
-  console.log('Ready to rumble!! 🚀');
+  console.log("Ready to rumble!! 🚀");
+
+  discordBot.hears("!dice", ["ambient"], (bot, message) => {
+    const embed = new discordBot.RichEmbed();
+    const clean = message.text.replace("!dice", "");
+    const d = clean.split("d");
+
+    const { result, allDice } = dice.roll(d[1].trim(), d[0].trim());
+
+    embed.addField("Rolled 🎲", result);
+    
+    for(var i = 0; i < allDice.length; i++) {
+      const idx = parseFloat(i) + 1;
+      embed.addField("Dice " + idx + " rolled:", allDice[i]);
+    }
+
+    embed.setColor("BLACK");
+
+    bot.reply(message, embed);
+  });
 
   discordBot.hears("Brolini?", ["ambient"], (bot, message) => {
     if (message.text.includes("?")) bot.reply(message, "Pasta Brolini! 🍝");
@@ -17,7 +56,7 @@ discordBot.on("ready", () => {
 
   discordBot.hears("!8ball", ["direct_message", "ambient"], (bot, message) => {
     const randomIndex = Math.floor(Math.random() * responses.length);
-  
+
     bot.reply(message, responses[randomIndex]);
   });
 });
