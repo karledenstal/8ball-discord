ede8a8799c5a251fcc101a84db7a3144760c3c09
Karl Edenstål <karl.edenstal@rhyme.se>
2021-10-20 17:02:39 +0200

    code

---
+++ index.js
@@ -0,0 +1,49 @@
+const discordBotkit = require('botkit-discord');
+
+const configuration = {
+    token: 'ODExMjk5MDc3NjQ3NDk5MjY1.YCwLJQ.znkLg5lVWrxv9XUAkrA_0LJLXTU'
+};
+
+const discordBot = discordBotkit(configuration);
+
+discordBot.hears("Brolini?", ["ambient"], (bot, message) => {
+  bot.reply(message, "Pasta Brolini!");
+});
+
+// The connector supports other types as well
+discordBot.hears('!8ball', ["direct_message", "ambient"], (bot, message) => {
+  const responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes – definitely",
+    "You may rely on it",
+    "As I see it",
+    "Yes",
+    "Most Likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+
+    "It is known",
+    "Leave me alone, bruh",
+    "Pasta Brolini",
+    ":nat:"
+  ];
+  
+  const randomIndex = Math.floor(Math.random() * responses.length);
+
+  bot.reply(message, "🎱 " + responses[randomIndex]);
+});
