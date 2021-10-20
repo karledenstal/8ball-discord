5ea25c9256393a6480a6ccc4de1c3e844048478d
Karl Edenstål <karl.edenstal@rhyme.se>
2021-10-20 17:15:47 +0200

    security fix!!!

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const discordBotkit = require('botkit-discord');
 require('dotenv').config();
+const responses = require("./src/responses");
+const discordBotkit = require("botkit-discord");
 
 const configuration = {
-    token: process.env.BOT_TOKEN
+  token: process.env.TOKEN
 };
 
 const discordBot = discordBotkit(configuration);
@@ -11,39 +12,12 @@ discordBot.hears("Brolini?", ["ambient"], (bot, message) => {
   bot.reply(message, "Pasta Brolini!");
 });
 
+discordBot.on("ready", () => {
+  console.log('Ready to rumble!!! 🚀');
+})
+
 // The connector supports other types as well
-discordBot.hears('!8ball', ["direct_message", "ambient"], (bot, message) => {
-  const responses = [
-    "It is certain",
-    "It is decidedly so",
-    "Without a doubt",
-    "Yes – definitely",
-    "You may rely on it",
-    "As I see it",
-    "Yes",
-    "Most Likely",
-    "Outlook good",
-    "Yes",
-    "Signs point to yes",
-
-    "Reply hazy, try again",
-    "Ask again later",
-    "Better not tell you now",
-    "Cannot predict now",
-    "Concentrate and ask again",
-
-    "Don't count on it",
-    "My reply is no",
-    "My sources say no",
-    "Outlook not so good",
-    "Very doubtful",
-
-    "It is known",
-    "Leave me alone, bruh",
-    "Pasta Brolini",
-    ":nat:"
-  ];
-  
+discordBot.hears("!8ball", ["direct_message", "ambient"], (bot, message) => {
   const randomIndex = Math.floor(Math.random() * responses.length);
 
   bot.reply(message, "🎱 " + responses[randomIndex]);
