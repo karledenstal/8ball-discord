98b06f6dee84bca815fede9b1a30b7701d024827
Karl Edenstål <karl.edenstal@rhyme.se>
2021-10-31 16:55:35 +0100

    revert

--- src/func/magic8ball.js
+++ src/func/magic8ball.js
@@ -2,8 +2,6 @@ const responses = require("../responses");
 
 module.exports = (discordBot) => {
   discordBot.hears("!8ball", ["direct_message", "ambient"], (bot, message) => {
-    console.log('message', message);
-    console.log('bot', bot);
     const randomIndex = Math.floor(Math.random() * responses.length);
 
     bot.reply(message, responses[randomIndex]);
